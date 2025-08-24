8546fac2672e6b4f019fbfe0320649bfbc51c160
Mael <araujoismael0982@gmail.com>
2025-08-24 02:25:46 -0300

    Meu primeiro SCRIPT do projeto em conjunto com HTML e CSS

--- javascript/script001.js
+++ javascript/script001.js
@@ -1,4 +1,21 @@
-let final_price = 0
-let product_name = ""
-let amount_of_product = 0
-let product_price = 0
+let button = document.getElementById("button")
+button.addEventListener("click", Calcular)
+let message = document.createElement("div")
+message.setAttribute("id", "msg")
+document.querySelector("section").appendChild(message)
+message.style.display = "none"
+
+function Calcular() {
+    message.style.display = "block"
+    let productName = (document.getElementById("iname").value.trim())
+    let productPrice = Number(document.getElementById("iprice").value)
+    let productAmount = Number(document.getElementById("iamount").value)
+    
+    if (productName == "" || productPrice <= 0 || Number.isNaN(productPrice) || productAmount <= 0 || Number.isNaN(productAmount)) {
+        message.style.color = "red"
+        message.innerHTML = "ERROR. Fill in correctly!"
+    } else {
+        let totalPrice = productPrice * productAmount
+        message.innerHTML = `Product Name: ${productName} <br> Product Price: R$ ${productPrice.toFixed(2)} <br> Product Amount: ${productAmount.toFixed(1)} <br> Total Price: R$ ${totalPrice.toFixed(2)}`
+    }
+}
